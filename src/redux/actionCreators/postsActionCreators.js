79a6f1f6dafb6240011b41d2d641f3aa2dcfae4e
KindCoffeBear <conter123@list.ru>
2022-04-17 14:08:50 +0300

    Внес пару доработок

--- src/redux/actionCreators/postsActionCreators.js
+++ src/redux/actionCreators/postsActionCreators.js
@@ -47,13 +47,18 @@ const deletePost = (id) => ({
 
 // удаление поста по id
 export const deletePostQuery = (id, token) => async (dispatch) => {
-  const response = await axiosInstance.delete(
-    `posts/${id}`,
-    { headers: { Authorization: `Bearer ${token}` } },
-  )
+  try {
+    await axiosInstance.delete(
+      `posts/${id}`,
+      { headers: { Authorization: `Bearer ${token}` } },
+    )
 
-  if (response.status === 200) {
     dispatch(deletePost(id))
+  } catch (e) {
+    const codeError = e.message.slice(-3)
+    if (codeError === '403') {
+      alert('Вы не можете удалить чужой пост')
+    }
   }
 }
 

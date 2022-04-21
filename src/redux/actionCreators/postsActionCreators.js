6ab8194218ad4eef781c593a7b76a7745aba935e
KindCoffeBear <conter123@list.ru>
2022-04-22 00:42:32 +0300

    на гитхаб

--- src/redux/actionCreators/postsActionCreators.js
+++ src/redux/actionCreators/postsActionCreators.js
@@ -4,7 +4,7 @@
 // не забыть вернуть GET_CURRENT_POST UPDATE_POST
 import axiosInstance from '../../axiosConfig/axiosConfig'
 import {
-  ADD_NEW_POST, DELETE_POST, GET_CURRENT_POST, GET_POSTS_FROM_SERVER, UPDATE_POST,
+  ADD_NEW_POST, DELETE_POST, GET_POSTS_FROM_SERVER, LIKE_POST,
 } from '../actionTypes/postsTypes'
 
 const getPostsFromServer = (postsFromServer) => ({
@@ -60,38 +60,28 @@ export const deletePostQuery = (id) => async (dispatch) => {
   }
 }
 
-const updatePost = (editedPost) => ({
-  type: UPDATE_POST,
-  payload: editedPost,
+const addLike = (data) => ({
+  type: LIKE_POST,
+  payload: data,
 })
 
-// обновление поста на сервере и получение данных с сервера
-export const updatePostQuery = (id, editedPost, closeModal) => async (dispatch) => {
-  const response = await axiosInstance.patch(
-    `posts/${id}`,
-    editedPost,
+export const addLikeQuery = (idPost) => async (dispatch) => {
+  const response = await axiosInstance.put(
+    `posts/likes/${idPost}`,
   )
-  if (response.status === 200) {
-    const updatedPostFromServer = response.data
-    dispatch(updatePost(updatedPostFromServer))
-    closeModal()
-  } else {
-    alert('Возможно, вы пытаетесь изменить пост, который делали не вы. Пожалуйста, выберите один из ваших')
-  }
+  const likesFromServer = await response.data
+  dispatch(addLike(likesFromServer))
 }
 
-const getPost = (postFromServer) => ({
-  type: GET_CURRENT_POST,
-  payload: postFromServer,
+const deleteLike = (likesFromServer) => ({
+  type: LIKE_POST,
+  payload: likesFromServer,
 })
 
-// получение конкретного поста по id и передача setLoading (изменение состояния загрузки страницы) и controller для отмены загрузки страницы
-export const getPostQuery = (idPost, setLoading, controller) => async (dispatch) => {
-  const response = await axiosInstance.get(
-    `posts/${idPost}`,
-    { signal: controller.current.signal },
-  ) // { signal: controller.current.signal } определяет идет запрос или он отменен
-  const postFromServer = response.data
-  dispatch(getPost(postFromServer))
-  setLoading(false)
+export const deleteLikeQuery = (idPost) => async (dispatch) => {
+  const response = await axiosInstance.delete(
+    `posts/likes/${idPost}`,
+  )
+  const likesFromServer = await response.data
+  dispatch(deleteLike(likesFromServer))
 }
